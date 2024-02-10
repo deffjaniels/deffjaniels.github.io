06da30a99f0145e8c95056ace2d95cf6e3bb5f9b
deffjaniels <107448750+deffjaniels@users.noreply.github.com>
2024-02-10 14:32:24 -0500

    test fisheye fix

--- motherload.js
+++ motherload.js
@@ -94,7 +94,7 @@ let gameStartState = {
     overallFuelModifier: 1,
     playerRelicArray: [],
     storeUpgradeArray: [],
-    fishEyeLens: false,
+    fishEyeLens: true,
 
     storeRelics: [],
     mapRelic1: false,
